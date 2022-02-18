7623ca7e18c192f420f555137e57abaceec0ca46
Shawn Faber <sfaber02@gmail.com>
2022-02-18 02:29:36 -0500

    added letter graphics and changed letter blank mechanics

---
+++ src/graphics/board-graphics.js
@@ -0,0 +1,17 @@
+const boardGraphics = [
+    require('./0.png'),
+    require('./1.png'),
+    require('./2.png'),
+    require('./3.png'),
+    require('./4.png'),
+    require('./5.png'),
+    require('./6.png'),
+    require('./7.png'),
+]
+
+const letterGraphics = [
+    require('./blank-space.png')
+];
+
+
+export default boardGraphics;
---
+++ src/graphics/letter-graphics.js
@@ -0,0 +1,32 @@
+const letterGraphics = {
+    blank: require('./blank-space.png'),
+    a: require('./a.png'),
+    b: require('./b.png'),
+    c: require('./c.png'),
+    d: require('./d.png'),
+    e: require('./e.png'),
+    f: require('./f.png'),
+    g: require('./g.png'),
+    h: require('./h.png'),
+    i: require('./i.png'),
+    j: require('./j.png'),
+    k: require('./k.png'),
+    l: require('./l.png'),
+    m: require('./m.png'),
+    n: require('./n.png'),
+    o: require('./o.png'),
+    p: require('./p.png'),
+    q: require('./q.png'),
+    r: require('./r.png'),
+    s: require('./s.png'),
+    t: require('./t.png'),
+    u: require('./u.png'),
+    v: require('./v.png'),
+    w: require('./w.png'),
+    x: require('./x.png'),
+    y: require('./y.png'),
+    z: require('./z.png'), 
+};
+
+
+export default letterGraphics;
