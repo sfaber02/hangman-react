9246fa1505908f6f1676d0a7299bb22d28010954
Shawn Faber <sfaber02@gmail.com>
2022-02-21 01:00:03 -0500

    finished peristent highscore tracking via local storage

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -5,29 +5,26 @@ const LOCAL_STORAGE_KEY = 'hangman.highscores';
 const ScoreBoard = (props) => {
     const score = props.scoreLives.score;
     const lives = props.scoreLives.lives;
-    const status = props.scoreLives.status;
     const turn = props.turn;
-    let highScore = useRef(0);
+    const [highScoreList, setHighScoreList] = useState(() => ({score: 0}));
 
-    // const [highScoreList, setHighScoreList] = useState(() => ({score: 0}));
+    useEffect(() => {
+        const highScoreData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if (highScoreData) {
+           setHighScoreList(highScoreData);
+        }
+    }, []);
 
-    // useEffect(() => {
-    //     const highScoreData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    //     if (highScoreData) {
-    //         // highScore.current = highScoreData;
-    //     }
-    // }, []);
+    useEffect (() => {
+        if (score > highScoreList.score) {
+            setHighScoreList({score: score});
+        }
+    }, [score]);
 
-    // useEffect (() => {
-    //     console.log (highScore.current);
-    //     if (score > highScore.current) {
-    //         highScore.current = score;
-    //     }
-    // }, [score]);
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(highScoreList));
+    }, [highScoreList]);
 
-    if (score > highScore.current) {
-        highScore.current = score;
-    }
     
 
 
@@ -36,7 +33,7 @@ const ScoreBoard = (props) => {
             <h3 className='scoreInfo'>Score: {score}</h3>
             <h3 className='scoreInfo'>Lives: {lives}</h3>
             <h3 className='scoreInfo'>Tries: {7 - turn}</h3>
-            <h3 className='scoreInfo'>High Score: {highScore.current}</h3>
+            <h3 className='scoreInfo'>High Score: {highScoreList.score}</h3>
         </div>
     );
 }
